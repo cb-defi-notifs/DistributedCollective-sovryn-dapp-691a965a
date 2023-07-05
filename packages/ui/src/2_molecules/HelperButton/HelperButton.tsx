0005de3b67c61e7018edeed4698ec47f8e9f37e4
pietro-maximoff <pietro.maxim.official@protonmail.com>
2023-07-05 16:30:29 +0200

    chore: updated stake page

--- packages/ui/src/2_molecules/HelperButton/HelperButton.tsx
+++ packages/ui/src/2_molecules/HelperButton/HelperButton.tsx
@@ -9,18 +9,21 @@ import styles from './HelperButton.module.css';
 type HelperButtonProps = {
   content: ReactNode;
   className?: string;
+  tooltipClassName?: string;
   dataAttribute?: string;
 };
 
 export const HelperButton: React.FC<HelperButtonProps> = ({
   content,
   className,
+  tooltipClassName,
   dataAttribute,
 }) => (
   <Tooltip
     content={content}
     className={classNames(styles.wrapper, className)}
     dataAttribute={dataAttribute}
+    tooltipClassName={tooltipClassName}
   >
     <div>
       <Icon size={12} icon="info" />
